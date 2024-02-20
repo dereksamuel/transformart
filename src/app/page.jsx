b9b3c193821345f25b2826889420fbdf1fcceb7f
Derek Paul <derek.paul@brilliant360.net>
2024-02-20 10:12:59 -0500

    fonts was added

--- src/app/page.jsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <main className="p-5">asasas</main>
-  );
-}
---
+++ src/pages/_app.jsx
@@ -0,0 +1,6 @@
+import "@/assets/fonts/stylesheet.css";
+import "./globals.css";
+
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
