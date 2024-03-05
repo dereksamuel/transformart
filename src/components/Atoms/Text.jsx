a273b6c4082794e9bb01b101565fd05881d58dab
Derek Paul <derek.paul@brilliant360.net>
2024-03-05 13:50:40 -0500

    restart design

--- src/components/Atoms/Text.jsx
+++
@@ -1,5 +0,0 @@
-export default function Text({element = <p></p>, children, ...anotherProps}) {
-  return (
-    <element>{children}</element>
-  );
-}
--- src/components/Atoms/WaveBody.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./css/wave-body.module.css";
-
-export default function WaveBody({ children }) {
-  return (
-    <div className={`p-5 ${styles["wave-body"]}`}>{children}</div>
-  );
-}
